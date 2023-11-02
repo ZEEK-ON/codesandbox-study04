8bc73a5f57949e082e428a309dba2f895383d2a1
イワシの匠 <44769766+ZEEK-ON@users.noreply.github.com>
2023-11-02 11:41:09 +0000

    登録される配列のkey（登録名）が一緒の時にエラーを吐くが一旦TODOアプリ完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useState } from "react/cjs/react.production.min";
 import "./styles.css";
+import { InputTodo } from "./components/inputTodo";
+import { InCompleteTodos } from "./components/incompleteTodos";
+import { CompleteTodos } from "./components/completeTodos";
 
 export const App = () => {
-  return <div></div>;
+  const [todoText, setTodoText] = useState("");
+  const [inCompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...inCompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...inCompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newInCompleteTodos = [...inCompleteTodos];
+    newInCompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, inCompleteTodos[index]];
+    setIncompleteTodos(newInCompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newInCompleteTodos = [...inCompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newInCompleteTodos);
+  };
+
+  return (
+    <>
+      <InputTodo
+        disabled={inCompleteTodos.length >= 5}
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      {inCompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodo5個までだよ～。消化しろ～。
+        </p>
+      )}
+      <InCompleteTodos
+        todos={inCompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
 };
